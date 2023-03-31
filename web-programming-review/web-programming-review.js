0a1b44c160bf33acabb318740234b5fe0e594cdf
Taemin <user732@a.ut.ac.kr>
2023-03-31 11:25:16 +0900

    202303311124

--- web-programming-review/web-programming-review.js
+++ web-programming-review/web-programming-review.js
@@ -3,10 +3,11 @@
  * 
  * 메뉴 버튼과 반응형 메뉴를 생성하십시오.
  */
+// alert("JS loaded!");
 
 // @TODO: querySelector 및 getElementById를 사용하여 관련 메뉴 노드를 가져오십시오.
-const menu;
-const menuButton;
+const menu ="";
+const menuButton = "";
 
 // @TODO: 반응형 메뉴를 열고 닫는 toggleMenu 함수를 작성하십시오.
 
